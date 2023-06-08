1ee449c8334c1417d0643d030ba0e0a6c7e106d2
Abhijit Vadrevu <avadrevu@ucsd.edu>
2023-06-08 01:36:08 -0700

    changing data routes

--- Server_code/app/src/routers/data_routes.py
+++ Server_code/app/src/routers/data_routes.py
@@ -42,7 +42,6 @@ def calculate_driving_score(request: Request, user: UserLogin=Depends(manager)) 
     global is_driving
     timestamp = is_driving.pop(user.user_id)
     driving_data = select_driving_data_range(user.user_id, 1686200314, 1686200521)
-    print(driving_data)
     trip_stats = calcTripStats(driving_data)
 
     result = insert_into_trip_stats(trip_stats, user.user_id)
